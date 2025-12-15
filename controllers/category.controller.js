70ded4567819f8512882d877fef32e797f305076
Nacer-atoui <nacer.atoui@laplateforme.io>
2025-12-15 14:48:11 +0100

    feat(category): ajout des fonctionalités getAll sur les  trois fichiers MVC

---
+++ controllers/category.controller.js
@@ -0,0 +1,17 @@
+const Category = require("../models/category.model");
+
+const getAllCategories = (req, res) => {
+    Category.findAll((error, results) => {
+        if (error) {
+            console.error("❌ Erreur lors de la requête SQL:", error.message);
+            return res.status(500).send("Erreur serveur");
+        }
+        res.json(results);
+    });
+};
+
+
+
+module.exports = {
+    getAllCategories,
+};
---
+++ models/category.model.js
@@ -0,0 +1,12 @@
+const db = require("../config/database");
+
+const findAll = (callback) => {
+    const sql = "SELECT * FROM category";
+    db.query(sql, callback);
+};
+
+
+// Ajouter autres requêtes du CRUD
+module.exports = {
+    findAll,
+};
